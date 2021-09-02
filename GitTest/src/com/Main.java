a4692be5fecc6f261ab2c795d9f35507767ffec0
Diego <diegoschroeder95@gmail.com>
2021-09-02 18:17:11 -0300

    added hungry prompt

--- GitTest/src/com/Main.java
+++ GitTest/src/com/Main.java
@@ -1,10 +1,24 @@
 package com;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("I'm hungry");
+		//System.out.println("I'm hungry");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Are you hungry?");
+		
+		String answer = scanner.nextLine();		
+		
+		if (answer.equals ("yes")) {
+			System.out.println("eat samosa");
+		} else {
+			System.out.println("do your homework");
+		}
 	}
 
 }
